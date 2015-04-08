37ffed66363a0ef097c088421073cab6dd81df81
mbaldassaro <mbaldassaro@gmail.com>
2015-04-08 15:30:15 -0400

    init commit
    grunt-bower dependencies, index.html file

---
+++ ~npm/lib/node_modules/generator-bootstrap/readme.md
@@ -0,0 +1,36 @@
+# Bootstrap generator [![Build Status](https://secure.travis-ci.org/yeoman/generator-bootstrap.svg?branch=master)](http://travis-ci.org/yeoman/generator-bootstrap)
+
+Simply lets you choose which flavor of [Bootstrap](http://getbootstrap.com) you would like to add to a project: CSS, SASS, LESS or Stylus. When you select an option, we'll pull down the relevant package from [Bower](http://bower.io) for you. 
+
+Maintainer: [Brian Ford](https://github.com/btford)
+
+## Preview
+
+```shell
+$ yo bootstrap
+[?] In what format would you like the Bootstrap stylesheets? (Use arrow keys)
+❯ css
+  sass
+  less
+  stylus 
+```
+
+## Getting Started
+
+Install: `npm install -g generator-bootstrap`
+
+Usage: `yo bootstrap`
+
+Available generators:
+
+- bootstrap:app
+
+
+## Contribute
+
+See the [contributing docs](https://github.com/yeoman/yeoman/blob/master/contributing.md)
+
+
+## License
+
+[BSD license](http://opensource.org/licenses/bsd-license.php)
---
+++ ~npm/lib/node_modules/generator-bootstrap/test/test.js
@@ -0,0 +1,78 @@
+/*global describe, beforeEach, it */
+'use strict';
+var path = require('path');
+var helpers = require('yeoman-generator').test;
+var assert = require('assert');
+
+describe('Bootstrap generator test', function () {
+  beforeEach(function (done) {
+    this.bowerInstallCalls = [];
+
+    helpers.testDirectory(path.join(__dirname, 'temp'), function (err) {
+      if (err) {
+        done(err);
+        return;
+      }
+
+      this.app = helpers.createGenerator('bootstrap:app', [
+        '../../app'
+      ]);
+
+      // Mock bower install and track the function calls.
+      this.app.bowerInstall = function () {
+        this.bowerInstallCalls.push(arguments);
+      }.bind(this);
+
+      done();
+    }.bind(this));
+  });
+
+  it('the generator can be required without throwing', function () {
+    // not testing the actual run of generators yet
+    this.app = require('../app');
+  });
+
+  it('installs bootstrap', function (done) {
+    helpers.mockPrompt(this.app, {
+      format: 'css'
+    });
+
+    this.app.run(function () {
+      assert.equal(this.bowerInstallCalls[0][0], 'bootstrap');
+      done();
+    }.bind(this));
+  });
+
+  it('installs sass-bootstrap', function (done) {
+    helpers.mockPrompt(this.app, {
+      format: 'sass'
+    });
+
+    this.app.run(function () {
+      assert.equal(this.bowerInstallCalls[0][0], 'bootstrap-sass-official');
+      done();
+    }.bind(this));
+  });
+
+  it('installs components-bootstrap', function (done) {
+    helpers.mockPrompt(this.app, {
+      format: 'less'
+    });
+
+    this.app.run(function () {
+      assert.equal(this.bowerInstallCalls[0][0], 'components-bootstrap');
+      done();
+    }.bind(this));
+  });
+
+  it('installs bootstrap-stylus', function (done) {
+    helpers.mockPrompt(this.app, {
+      format: 'stylus'
+    });
+
+    this.app.run(function () {
+      assert.equal(this.bowerInstallCalls[0][0], 'bootstrap-stylus');
+      done();
+    }.bind(this));
+  });
+});
